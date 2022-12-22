53d805b2df8c803775ae235374262f26f038a82c
Antonio <antoniovgmx@gmail.com>
2022-12-21 23:51:19 -0600

    changing port

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.use('/', require('./routes/views'));
 // API Routes
 app.use('/api/', require('./routes/api'));
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
